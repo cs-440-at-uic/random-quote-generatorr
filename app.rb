01a82c26982f6a1e6b696e1e3e18ad47829778d3
Chandhu Bhumireddy <89249579+bchandhu@users.noreply.github.com>
2024-05-17 17:45:41 +0000

    api issue resolved

--- app.rb
+++ app.rb
@@ -1,9 +1,21 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
+require "json"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:homepage)
 end
+
+post("/quote") do
+  @category = params[:genre]
+  api_url = "https://api.api-ninjas.com/v1/quotes?category=#{@category}"
+  puts "API URL: #{api_url}"  # Debugging
+  response = HTTP.headers(:'X-Api-Key' => ENV['API_NINJA_KEY']).get(api_url)
+  puts "Response Body: #{response.body}"  # Debugging
+  @api_key = ENV['API_NINJA_KEY']
+  @quotes = JSON.parse(response.body)
+  puts "Quotes: #{@quotes}"  # Debugging
+  erb(:quote)
+end
+
